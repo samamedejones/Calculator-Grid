073acc089033a8bef9eda556656f14099fe0f2e5
Jones <samamedejones@gmail.com>
2024-10-31 16:33:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function numberClick(caracter) {
+  const display = document.querySelector('.display').value
+  document.querySelector('.display').value = display + caracter
+
+}
+
+function resetDisplay() {
+  document.querySelector('.display').value = ''
+} 
+
+function inverterSinal() {
+  const display = document.querySelector('.display').value
+
+  document.querySelector('.display').value = display * -1
+}
+
+function calcular() {
+  const display = document.querySelector('.display').value
+  document.querySelector('.display').value = eval(display)
+}
+
+document.addEventListener('keydown', function (event) {
+  console.log(event.key)
+  if(event.key === 'Enter'){
+    calcular()
+  } 
+})
